b2df7d04ba8955b59edc9bdd8be62163093d3906
VeaG1 <veanag1@learn.cssd.ab.ca>
2026-05-21 09:57:21 -0600

    i changes thing and it be changes

--- plyr.js
+++ plyr.js
@@ -30,10 +30,10 @@ export function playerActions(boxes) {
   camera.y = lerp(camera.y, plyr.y, 0.2);
 
   // Movement
-  if (keyIsDown("w")) plyr.vel.y -= 2;
-  if (keyIsDown("s")) plyr.vel.y += 2;
-  if (keyIsDown("a")) plyr.vel.x -= 2;
-  if (keyIsDown("d")) plyr.vel.x += 2;
+  if (keyIsDown("w")) plyr.vel.y -= 4;
+  if (keyIsDown("s")) plyr.vel.y += 4;
+  if (keyIsDown("a")) plyr.vel.x -= 4;
+  if (keyIsDown("d")) plyr.vel.x += 4;
 
   // Grabbing / Dropping
   if (kb.presses("e")) {
--- sketch.js
+++ sketch.js
@@ -15,33 +15,39 @@ smallBox.drag = 5
 smallBox.mass = 2
 smallBox.scale = 0.6
 smallBox.rotationDrag = 5
+smallBox.layer = 2
+
 
 
 let borderY1 = new Sprite();
 borderY1.x = 0;
 borderY1.y = -800;
-borderY1.width = 1600;
+borderY1.w = 1600;
+borderY1.h = 100;
 borderY1.rotation = 0;
 borderY1.physics = STATIC;
 
 let borderY2 = new Sprite();
 borderY2.x = 0;
 borderY2.y = 800
-borderY2.width = 1600;
+borderY2.w = 1600;
+borderY2.h = 100;
 borderY2.rotation = 0;
 borderY2.physics = STATIC;
 
 let borderX1 = new Sprite();
 borderX1.x = 800;
 borderX1.y = 0;
-borderX1.width = 1600;
+borderX1.w = 1600;
+borderX1.h = 100;
 borderX1.rotation = 90;
 borderX1.physics = STATIC;
 
 let borderX2 = new Sprite();
 borderX2.x = -800;
 borderX2.y = 0;
-borderX2.width = 1600;
+borderX2.w = 1600;
+borderX2.h = 100;
 borderX2.rotation = 90;
 borderX2.physics = STATIC;
 
